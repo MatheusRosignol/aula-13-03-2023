7b95b9e79cfa9369cc7cfc4318e7347602932d70
MatheusRosignol <matheusrosig5@gmail.com>
2023-03-13 20:39:51 -0300

    Primeiro commit

---
+++ while2.py
@@ -0,0 +1,12 @@
+contador = 0
+
+while contador<= 100:
+    
+    contador = contador + 1 
+    
+    if contador == 24:
+        print("Não vou exibir o número 24")
+        print("Acabou")
+        continue
+    print(contador)
+    
