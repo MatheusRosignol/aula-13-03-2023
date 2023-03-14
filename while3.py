c870fc29969c82a024e54848243fe07353bdc118
MatheusRosignol <matheusrosig5@gmail.com>
2023-03-13 21:07:15 -0300

    Primeiro commit

---
+++ while3.py
@@ -0,0 +1,7 @@
+numero = int(input("Escreva o número da tabuada de preferência: "))
+quantidade = 0
+
+while quantidade < 10:
+    #quantidade = quantidade +1
+    quantidade +=1
+    print(f"{quantidade} x {numero} = {quantidade * numero}")
