efa8b717976214a05d7d906c859a13ad3dcea847
MatheusRosignol <matheusrosig5@gmail.com>
2023-03-13 22:03:01 -0300

    Calculadora concluída

--- calculadora.py
+++ calculadora.py
@@ -1,18 +1,15 @@
 sair = "sair"
 
-sair = input("Deseja sair? ").lower().startswith("s")
+
+
 
 while True:
     numero1 = float(input ("Escreve o primeiro númmero: "))
     numero2 = float(input ("Escreve o segundo númmero: "))
     operador =input("Digite o operador (+_*/): ")
     
-    numero1_float = 0
-    numero2_float = 0
     
     try: # Tentar executar o código
-        numero1_float = float(numero1)
-        numero2_float = float(numero2)
         numero_valido = True
     
     except: # Caso tenha executar o código abaixo   
@@ -23,9 +20,9 @@ while True:
         continue        
         
         
-    operador_aceito = ("+-*/")
-    operador not in operador_aceito
-    print("Operador não aceito")
+    # operador_aceito = ("+-*/")
+    # if operador not in operador_aceito:
+    #     print("Operador não aceito")
     
     if operador == "+":
         print(f"Resultado: {numero1 + numero2}")
@@ -35,7 +32,8 @@ while True:
         print(f"Resultado: {numero1 * numero2}")
     elif operador == "/":
         print(f"Resultado: {numero1 / numero2}")        
-     
+    else:
+        print("Operador inválido.")
         
     sair = input("Deseja sair? ").lower().startswith("s")
     if sair is True:
